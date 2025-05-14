95814fea5e0d9f6b15a3ab28ff34c936ac25e6ac
Abhishek Patria <patriaabhishek@gmail.com>
2025-05-14 12:28:20 -0400

    correct typo

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -142,7 +142,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
-# TheFUck
+# TheFuck
 sudo apt update
 sudo apt install -y python3-setuptools thefuck
 
