bbf5590c8f8fc4166dba7bca95496fd4204c20c1
Abhishek Patria <patriaabhishek@gmail.com>
2026-03-02 23:03:27 -0500

    cleanup linux installation script

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,15 +1,3 @@
-# Add new repo
-sudo add-apt-repository universe
-
-# Install basic stuff
-sudo apt install -y coreutils software-properties-common wget curl openssl cmake jq grc tree tmux redis watchman python3-dev python3-pip libncurses-dev gcc unzip nano zsh vim build-essential python3-setuptools cmake figlet
-
-# Firacode
-sudo apt install fonts-firacode
-
-#Ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Remove old version of Docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
@@ -47,58 +35,6 @@ sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as c
 sudo apt-get update
 sudo apt-get install -y kubectl
 
-#Eza
-sudo apt install -y gpg
-sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
-sudo apt install -y eza
-
-# OpenSSH Server
-sudo apt install -y openssh-server
-
-# Nmap
-sudo apt install -y nmap
-
-# Fzf 
-sudo apt install -y fzf
-
-# AWS CLI
-sudo snap install aws-cli --classic
-
 # Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
-# Miniconda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm ~/miniconda3/miniconda.sh
-source miniconda3/bin/activate
-conda init --all
-
-#Downloading plugins to install
-
-git clone https://github.com/bobthecow/git-flow-completion ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-flow-completion
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
-# TheFUck
-sudo apt update
-sudo apt install python3-setuptools
-sudo apt install thefuck
-
-# Pipx (Ensure this is installed after Miniconda)
-sudo apt update
-sudo apt install pipx
-pipx ensurepath
-sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
-
-# Poetry (Ensure this is installed after Miniconda)
-pipx install poetry
