757c4e0d40c2bb6d65ae8f775b12bfce576584c7
Abhishek Patria <patriaabhishek@gmail.com>
2026-03-02 22:57:54 -0500

    move vim to postcreate

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -100,6 +100,18 @@ rm -R configure_nano
 # Configure VIM #
 #################
 
-cd vim
-./config_vim.sh
-cd ..
+#Installing Vim
+sudo apt install -y vim
+
+#Copy .vimrc
+cp .vimrc ~/.vimrc
+
+#Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#Install Plugins
+vim --clean '+source ~/.vimrc' +PluginInstall +qall
+
+#Install YouCompleteMe
+cd ~/.vim/bundle/youcompleteme
+python3 install.py --all
